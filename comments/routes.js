9ea0e8da850fdc563a29921f119a47f944c35dc4
gbrds <joosep.holdt@gmail.com>
2025-11-10 15:44:01 +0200

    Docker Setup

--- comments/routes.js
+++ comments/routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 let comments = [];
 
-const POSTS_API = 'http://localhost:3001/posts';
+const POSTS_API = 'http://posts:3001/posts';
 
 // Get comments for a post
 router.get('/:postId', async (req, res) => {
@@ -41,7 +41,7 @@ router.post('/:postId', async (req, res) => {
     comments.push(newComment);
 
     try {
-        await axios.post('http://localhost:3003/events', {
+        await axios.post('http://events:3003/events', {
             type: 'CommentCreated',
             data: newComment
         })
